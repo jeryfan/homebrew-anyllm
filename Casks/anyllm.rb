fc3b11918769f2e341fb7c327179cf1dacc3f56f
jeryfan <63136443+jeryfan@users.noreply.github.com>
2026-02-15 01:12:07 +0000

    chore: bump to v0.1.0

--- Casks/anyllm.rb
+++ Casks/anyllm.rb
@@ -1,7 +1,7 @@
 cask "anyllm" do
-  version "0.2.0"
-  sha256 arm:   "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
-         intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.1.0"
+  sha256 arm:   "2be4d8f9aefc976d9a1ec35e67c6858421c6d19229a494c219d0c7d5ac0f7945",
+         intel: "fe5761f16535a4a78c29366cc2195b9790a3296d89f344a2479855395b1881f8"
 
   on_arm do
     url "https://github.com/jeryfan/anyllm/releases/download/v#{version}/AnyLLM_#{version}_aarch64.dmg"
