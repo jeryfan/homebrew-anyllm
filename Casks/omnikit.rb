fcda2ef7a9f2fcda8d5c5b29c955cf5c4400e0e7
jeryfan <63136443+jeryfan@users.noreply.github.com>
2026-02-20 01:12:25 +0000

    chore: bump to v0.3.0

--- Casks/omnikit.rb
+++ Casks/omnikit.rb
@@ -1,7 +1,7 @@
 cask "omnikit" do
-  version "0.2.0"
-  sha256 arm:   "976bf5deb3fcc73881817300f2af1c07ce32b1a163198142d532aeb49b5d8b58",
-         intel: "80d7201caff261def98f8cc2dad8b989b512bfa83a1c03bba0eea323c8a383cc"
+  version "0.3.0"
+  sha256 arm:   "64454fc2c5a3309af62622097156af76c6783671bb79a1ba5bf8295ea438b4e1",
+         intel: "27379b6de8814c79e5d2b1068bbffe5455a8f0d9595bffe19f8367ead351b096"
 
   on_arm do
     url "https://github.com/jeryfan/omnikit/releases/download/v#{version}/OmniKit_#{version}_aarch64.dmg"
